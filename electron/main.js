2ab49017c3a3edb711271fa9f6a6cb44c5ed2b9f
Molloy <2753219155@qq.com>
2024-04-02 20:12:26 +0800

    1

--- electron/main.js
+++ electron/main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron/main')
-const path = require('node:path')
+const path = require('path')
+const fs = require("fs");
 
 function createWindow () {
     const win = new BrowserWindow({
@@ -28,3 +29,27 @@ app.on('window-all-closed', () => {
         app.quit()
     }
 })
+
+
+const directoryPath = 'C:\\app\\Code\\Frontent\\ev\\src\\components';
+
+fs.readdir(directoryPath, { withFileTypes: true,recursive:true }, (err, files) => {
+    if (err) {
+        console.error('Error reading directory:', err);
+        return;
+    }
+    console.log(files)
+
+    const directories = files.filter(file => file.isDirectory()).map(directory => directory.name);
+    console.log('Subdirectories:', directories);
+});
+
+// 使用 fs.readFile 读取文件内容
+fs.readFile('C:\\app\\Code\\Frontent\\ev\\src\\components\\TheWelcome.vue', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading file:', err);
+        return;
+    }
+    console.log('File content:', data);
+});
+
